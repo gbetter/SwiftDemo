edae4a8c69d49d2676bcb398d197d8c9a149e835
ZZY <1246133242@qq.com>
2017-05-08 20:32:07 +0800

    提交

---
+++ SwiftLearn/SwiftLearn/STLSurveyViewController.swift
@@ -0,0 +1,37 @@
+//
+//  STLSurveyViewController.swift
+//  SwiftLearn
+//
+//  Created by ZZY on 2017/5/8.
+//  Copyright © 2017年 STL. All rights reserved.
+//
+//  Swift 概览
+
+import UIKit
+
+class STLSurveyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title="Swift 概览"
+        self.view.backgroundColor=UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SwiftLearn/SwiftLearn/ViewController.swift
+++ SwiftLearn/SwiftLearn/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var values = ["Swift 概览","基础内容","基本运算符","字符串和字符","集合类型","控制流","函数","闭包","枚举"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,43 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return values.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let initIdentifier = "initIdentifier"
+        let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: initIdentifier)
+        cell.textLabel?.text = values[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let surveyConroller = STLSurveyViewController()
+        
+        self.navigationController?.pushViewController(surveyConroller, animated: true)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+//    override var prefersStatusBarHidden: Bool {
+//        return true
+//    }
+    
+    
 
 }
 
